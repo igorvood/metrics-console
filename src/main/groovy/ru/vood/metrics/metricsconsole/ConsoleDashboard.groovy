cca5d5b216ba9ad182e3c19031da79fc681fbdd2
vood <balitsky_igor@list.ru>
2022-03-17 19:46:02 +0300

    s

--- src/main/groovy/ru/vood/metrics/metricsconsole/ConsoleDashboard.groovy
+++ src/main/groovy/ru/vood/metrics/metricsconsole/ConsoleDashboard.groovy
@@ -6,8 +6,9 @@ import lombok.extern.slf4j.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import ru.vood.metrics.metricsconsole.service.FetchRatesService
 import ru.vood.metrics.metricsconsole.model.RateStatus
+import ru.vood.metrics.metricsconsole.service.FetchRatesService
+
 import java.util.concurrent.CompletableFuture
 
 import static java.lang.String.format
@@ -22,16 +23,26 @@ import static org.fusesource.jansi.Ansi.ansi
 class ConsoleDashboard {
     public static final int PAD = 20
 
-    @Autowired FetchRatesService fetchRatesService
-    @Autowired DashboardProperties properties
+    @Autowired
+    FetchRatesService fetchRatesService
+    @Autowired
+    DashboardProperties properties
 
     @Scheduled(fixedDelay = 50L)
     void run() {
-        def whenDone = sequence([
-                fetchRatesService.getRateStatus(properties.getLetterGrabberUrl()).thenApply({ status -> status.setComponent("pechkin-service") }),
-                fetchRatesService.getRateStatus(properties.getLetterSignatureUrl()).thenApply({ status -> status.setComponent("big-brother-service") }),
-                fetchRatesService.getRateStatus(properties.getGuardUrl()).thenApply({ status -> status.setComponent("agent-smith-service") })
-        ])
+        def each = properties.serviceMap.entrySet()
+                .collect { entry ->
+                    fetchRatesService.getRateStatus(entry.value).thenApply({ status -> status.setComponent(entry.key) })
+                }.toList()
+//        def whenDone = each
+
+//        def whenDone = sequence([
+//                fetchRatesService.getRateStatus(properties.getLetterGrabberUrl()).thenApply({ status -> status.setComponent("pechkin-service") }),
+//                fetchRatesService.getRateStatus(properties.getLetterSignatureUrl()).thenApply({ status -> status.setComponent("big-brother-service") }),
+//                fetchRatesService.getRateStatus(properties.getGuardUrl()).thenApply({ status -> status.setComponent("agent-smith-service") })
+//        ])
+        def whenDone = sequence(each)
+
 
         def builder = new StringBuilder()
         whenDone.thenAccept { statuses ->
@@ -41,18 +52,18 @@ class ConsoleDashboard {
             ansi().restoreCursorPosition()
 
             (statuses.size() + 3).times {
-                builder.append org.fusesource.jansi.Ansi.ansi().cursorUpLine().eraseLine()
+                builder.append ansi().cursorUpLine().eraseLine()
             }
 
             builder.append("┏${('━' * TABLE_LINESIZE)}┓\n")
             builder.append '┃'
-            builder.append org.fusesource.jansi.Ansi.ansi().fgBright(org.fusesource.jansi.Ansi.Color.BLACK)
+            builder.append ansi().fgBright(BLACK)
             builder.append 'service name'.padRight(COL0_MAXSIZE + 2)
             builder.append 'speed'.center(8)
             builder.append 'buffers'.center(12)
             builder.append 'workers'.center(4)
             builder.append ' '.padLeft(11)
-            builder.append org.fusesource.jansi.Ansi.ansi().reset()
+            builder.append ansi().reset()
             builder.append '┃\n'
 
             statuses.each { status ->
@@ -71,7 +82,7 @@ class ConsoleDashboard {
     private String formatBuffer(RateStatus status) {
         def buffer = status.buffers?.get(0)
         return buffer?.with {
-            def result = org.fusesource.jansi.Ansi.ansi()
+            def result = ansi()
 
             if (remaining <= maxSize * 0.75) {
                 result.fgBrightRed()
@@ -86,11 +97,11 @@ class ConsoleDashboard {
     }
 
     private String formatRate(RateStatus status) {
-        org.fusesource.jansi.Ansi.ansi().fgBrightCyan().format('%8.2f', status.getLetterRps()).reset().toString()
+        ansi().fgBrightCyan().format('%8.2f', status.getLetterRps()).reset().toString()
     }
 
     private String formatComponent(RateStatus status, int colSize) {
-        org.fusesource.jansi.Ansi.ansi().fgBrightGreen().a(status.getComponent().padRight(colSize + 2, '.')).reset().toString()
+        ansi().fgBrightGreen().a(status.getComponent().padRight(colSize + 2, '.')).reset().toString()
     }
 
     static CompletableFuture<List<RateStatus>> sequence(List<CompletableFuture<RateStatus>> futures) {
--- src/main/groovy/ru/vood/metrics/metricsconsole/DashboardProperties.java
+++ src/main/groovy/ru/vood/metrics/metricsconsole/DashboardProperties.java
@@ -3,10 +3,14 @@ package ru.vood.metrics.metricsconsole;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.HashMap;
+
 @Data
 @ConfigurationProperties(prefix = "dashboard")
 public class DashboardProperties {
     private String letterSignatureUrl;
     private String letterGrabberUrl;
     private String guardUrl;
+
+    private HashMap<String, String> serviceMap;
 }
