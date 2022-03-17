9bc883e44a952241624d987470e6ff4dc18f6c33
vood <balitsky_igor@list.ru>
2022-03-17 19:11:21 +0300

    s

---
+++ src/main/groovy/ru/vood/metrics/metricsconsole/MetricsConsoleApplication.groovy
@@ -0,0 +1,13 @@
+package ru.vood.metrics.metricsconsole
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MetricsConsoleApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(MetricsConsoleApplication, args)
+    }
+
+}
---
+++ src/test/groovy/ru/vood/metrics/metricsconsole/MetricsConsoleApplicationTests.groovy
@@ -0,0 +1,13 @@
+package ru.vood.metrics.metricsconsole
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MetricsConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
